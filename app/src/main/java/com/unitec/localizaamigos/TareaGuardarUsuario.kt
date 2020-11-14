b1ae8b7a91730c5d7a612489a6112c7017707452
juan_ <rapidclimate@gmail.com>
2020-11-13 19:57:09 -0600

    clasede hoy 10  de Noviembre

---
+++ app/src/main/java/com/unitec/localizaamigos/TareaGuardarUsuario.kt
@@ -0,0 +1,61 @@
+package com.unitec.localizaamigos
+
+import android.content.Context
+import android.os.AsyncTask
+import android.widget.Toast
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+class TareaGuardarUsuario(var contexto: Context?): AsyncTask<Void, Void, Void>() {
+
+    //Declaramos como atributos
+    var coordenada1=Coordenada(19.50,99.8)
+    var coordenada2=Coordenada(19.51,99.63)
+
+    var coordenadas= arrayListOf<Coordenada>(coordenada1, coordenada2)
+
+    var usuario=Usuario("campitos",
+        "Juan Carlos",
+        "Campos",
+        "rapidclimate@gmail.com",
+       coordenadas)
+
+    //Finalmete ponemos el objeto de respuesta (Estatus)
+    var estatus=Estatus()
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        //Este metodo sirve para pedir informacion a la interfaz de usuario
+        //Para hacerlo rapido crearemos un objeto fake
+
+    }
+
+    //Este metodo sirve para enviar al back la informacion en este caso, la info fake
+    //declarada arriba
+    override fun doInBackground(vararg p0: Void?): Void? {
+      //Aqui vamos a prparar nuestro usuario para enviarlo hacia el background con retrofit
+      //Paso 1: Crear un objeto de tipo retrofit
+        var retrofit =Retrofit.Builder()
+            .baseUrl("https://jc-unitec.herokuapp.com/")
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+
+        //PASO 2: GENERAR UN OBJETO PARAHABILITAR TU SERVICIO DE RETROFIT USANDO EL OBJETO
+        //DEL PUNTO ANTERIOR
+        var servicioUsuario=retrofit.create(ServicioUsuario::class.java)
+        //PASO 3: ENVIAR AL BACK -END
+        var envio=servicioUsuario.guardar(usuario)
+        //SE ENVIA  AL BACK- END Y  EN ESTE MOMENTO SE OBTIENE LA RESPUESTA
+        estatus=envio.execute().body()!!
+
+
+        return null
+    }
+
+    override fun onPostExecute(result: Void?) {
+        super.onPostExecute(result)
+        //FINALMENTE VAMOS A MOSTRAR EN PANTALLA (INTERFAZ DE USUARIO) EL ESTATUS DE NUESTRO
+        //BACK
+        Toast.makeText(contexto,estatus.mensaje, Toast.LENGTH_LONG ).show()
+    }
+}
