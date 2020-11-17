c73445583748d3c0d9fe4e2d60294ad888226966
juan_ <rapidclimate@gmail.com>
2020-11-17 13:36:04 -0600

    clasede hoy 17  de Noviembre

--- app/src/main/java/com/unitec/localizaamigos/MainActivity.kt
+++ app/src/main/java/com/unitec/localizaamigos/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.unitec.localizaamigos
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.mapbox.android.core.location.*
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
@@ -12,6 +15,7 @@ import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
@@ -25,182 +29,116 @@ import java.lang.ref.WeakReference
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var mapboxMap: MapboxMap
 
-   var  mapView: MapView?=null
-    var mapBoxMap:MapboxMap?=null
-    var permisosManager:PermissionsManager?=null
-    val INTERAVALO_DEFECTO=1000L
-    val TIEMPO_ESPERA=INTERAVALO_DEFECTO*5
-    var localizacionEngine: LocationEngine?=null
-
-    private var callback=LocalizacionActivity(this)
-
-    //Implementamos esa clasesisita
-    internal inner class LocalizacionActivity(activity:MainActivity):LocationEngineCallback<LocationEngineResult>{
-        //En esta parte espere el objeto de tipo callback para acceder a la localizacion en el rango establecido
-        private var activityWeak: WeakReference<MainActivity>?=null
-        init {
-            activityWeak= WeakReference(activity)
-        }
-
-        /******************************************************************************************************
-         * Tercer bloque de código agregamos los siguiente métodos para
-         */
-        //Aqui en  el metodo  onSuccess , es donde constantemenete se esta checando la localizacion en el rango
-        //que indicamos previamente en los atibutos.
-        override fun onSuccess(result: LocationEngineResult?) {
-
-
-            val activity: MainActivity? = activityWeak!!.get()
-            if (activity != null) {
-                val location = result?.lastLocation ?: return
-
-                // Cada vez que se actualice con exito una nueva localización,  aparecera este Toast
-                //Puedes eliminarlo si gustas para que no apareca cada rato y este mame y mame en la pantalla del usuario
-                //
-                Toast.makeText(
-                    activity,
-                    "Lat. actualizada:" + result.lastLocation!!.latitude.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                //La nueva posicion actualiada es la variable "pos"; los dobles signos de exclamacion en kotlin implican que este valor NO ES NULO
-                // esta posicion es la que debe de irse agregando a un ArrayList en tu Perfil.
-                //ES LA QUE NOS INTERESA PARA FINES DE MONITOREO O TRACKING DEL USUARIO!, HAY QUE PINCHE EMOCION!!
-                var pos = LatLng()
-                pos.latitude = result.lastLocation!!.latitude
-                pos.longitude = result.lastLocation!!.longitude
-// La variable posicion es la que va a  verse reflejada y atualizada en el mapa y lo trasladamos con el método:
-                //animateCamera, pudes cambiar el zoom de la camara y el metodo "tilt", es para poner que tan inclinado se muestra tu mapita
-                //para que simule que estas en 3D, es una mamada pero bueno!!, se ve chingon e impresiona a los no-ingenieros.
-                val posicion = CameraPosition.Builder()
-                    .target(pos)
-                    .zoom(18.0)
-                    .tilt(20.0)
-                    .build()
-
-                mapBoxMap?.animateCamera(CameraUpdateFactory.newCameraPosition(posicion), 500)
-
-                // Pasamos la nueva localizacion actualizada  al mapbox cada que se cumpla el rango de tiempo
-                if (activity.mapBoxMap != null && result.lastLocation != null) {
-                    activity.mapBoxMap!!.getLocationComponent()
-                        .forceLocationUpdate(result.lastLocation)
-                }
-            }
-        }
-
-
-    override fun onFailure(exception: Exception) {
-        TODO("Not yet implemented")
-    }
-}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      //La instanccia de delara ANTES  DE TU setContentView
-     // Mapbox.getInstance(this,"xxxxxx")
-     Mapbox.getInstance(this, getString(R.string.miToken))
 
 
-        mapView =findViewById(R.id.mapView) as MapView
+        Mapbox.getInstance(this, getString(R.string.miToken))
+
 
         setContentView(R.layout.activity_main)
-        //Paso 7 b.
-        mapView?.onCreate(savedInstanceState)
-        //Paso 7 c.
-        mapView?.getMapAsync(this)
 
-    }
-//Paso 7 d.
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        //Invocamos si es que se guardo, una instancia del ultimo mapa
-    mapView?.onSaveInstanceState(outState)
-
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        //Destruimos el mapa cuando ya no se este en la app
-        mapView?.onDestroy()
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.OUTDOORS) {
+
+            // Habilitamos la localizacion
+            enableLocationComponent(it)
+        }
     }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        //para baja memoria
-        mapView?.onLowMemory()
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        // Checams si los permisos de localizacion son otorgados
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            // Personalizacion de opciones de localizacion
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.colorAccent))
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+
+            // Obtenemos una instancia del a componente de localizacion
+            mapboxMap.locationComponent.apply {
+
+                // Activamos la localizacin con opciones
+                activateLocationComponent(locationComponentActivationOptions)
+
+                // Habilitamos si la localizacionesta en true
+                isLocationComponentEnabled = true
+
+                // ponemos el tracking activado
+                cameraMode = CameraMode.TRACKING
+
+                // ajustamos la brujula como visible
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        //Para pausar la activad del mapa en lo que haces otra cosa, por ejemplo contestar una llamada
-        mapView?.onPause()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        Toast.makeText(this, "Necesitas tu localizacion", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(mapboxMap.style!!)
+        } else {
+            Toast.makeText(this, "No conediste el permiso de localozacion", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
+        mapView.onStart()
     }
 
     override fun onResume() {
         super.onResume()
-        mapView?.onResume()
+        mapView.onResume()
     }
 
-    override fun onMapReady(mapboxMap: MapboxMap) {
-this.mapBoxMap=mapBoxMap
-        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-            Style.OnStyleLoaded {
-                 @Override fun onStyleLoaded(@NotNull style:Style){
-    habilitarLocalizacion(style)
-                 }
-            }
-        }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
     }
 
-    @SuppressWarnings("MissingPermission")
-    fun habilitarLocalizacion(@NotNull styleCargado:Style){
-     if(PermissionsManager.areLocationPermissionsGranted(this)){
-        var localizacionComponente=mapBoxMap?.locationComponent
-       var  locaActivacion=LocationComponentActivationOptions.builder(this,styleCargado)
-           .useDefaultLocationEngine(false)
-           .build()
-         //
-         localizacionComponente?.activateLocationComponent(locaActivacion)
-         //
-         localizacionComponente?.setLocationComponentEnabled(true)
-         //
-         localizacionComponente?.setCameraMode(CameraMode.TRACKING)
-         //
-         localizacionComponente?.setRenderMode(RenderMode.COMPASS)
-         initLocalizacionEngine()
-
-         }else{
-         permisosManager= PermissionsManager(this)
-         permisosManager?.requestLocationPermissions(this)
-     }
-    }
-    @SuppressWarnings("MissingPermission")
-    fun initLocalizacionEngine(){
-      localizacionEngine=LocationEngineProvider.getBestLocationEngine(this)
-     var  request=LocationEngineRequest.Builder(INTERAVALO_DEFECTO)
-         .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-         .setMaxWaitTime(TIEMPO_ESPERA).build()
-        localizacionEngine?.requestLocationUpdates(request, callback, mainLooper)
-        localizacionEngine?.getLastLocation(callback)
-
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
     }
 
-    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-        TODO("Not yet implemented")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
-    override fun onPermissionResult(granted: Boolean) {
-        TODO("Not yet implemented")
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
